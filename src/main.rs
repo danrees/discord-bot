ef8a819e96b21d4f44726fe14388837739d73f59
Daniel Rees <daniel_james_rees@hotmail.com>
2021-03-06 17:53:48 -0500

    Successful ping/status command

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,49 @@
+use serenity::client::{EventHandler,Context};
+use serenity::async_trait;
+use serenity::framework::standard::{macros::{
+    command,
+    group
+}, CommandResult};
 use std::env;
-use serenity::{
-  async_trait,
-    model::{channel::Message, gateway::Ready},
-    prelude::*,
-    framework::standard::macros::{command,group}
-};
 use serenity::framework::StandardFramework;
-use serenity::framework::standard::CommandResult;
-use serenity::http::routing::Route::UsersId;
+use serenity::Client;
+
+use serenity::model::channel::Message;
 
 #[group]
-#[description = "Basic functions against the valheim server"]
-#[commands(valheim_status)]
+#[commands(ping,status)]
 struct General;
 
 struct Handler;
 
 #[async_trait]
-impl EventHandler for Handler {
-
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected", ready.user.name)
-    }
-}
+impl EventHandler for Handler{}
 
 #[tokio::main]
 async fn main() {
-    dotenv::dotenv().ok();
-    let bot_id = env::var("BOT_ID").expect("You need to provide the bot id");
-
+    dotenv::dotenv().expect("could not read env file");
     let framework = StandardFramework::new()
-        .configure(|c| c
-            .with_whitespace(true)
-            .on_mention(Some(bot_id.parse().unwrap()))
-            .prefix("~")
-            .delimiters(vec![", ", ","])
-            )
+        .configure(|c| c.prefix("~"))
         .group(&GENERAL_GROUP);
-
-    let token = env::var("BOT_TOKEN")
-        .expect("You need to provide DISCORD_TOKEN");
-    let mut client = Client::builder(&token)
-        .framework(framework)
+    let token = env::var("BOT_TOKEN").expect("Need to supply BOT_TOKEN");
+    let mut client = Client::builder(token)
         .event_handler(Handler)
+        .framework(framework)
         .await
-        .expect("Err creating client");
-    
+        .expect("Error creating client");
     if let Err(why) = client.start().await {
-        println!("Client err: {:?}", why)
+        println!("An error occurred while running the client {:?}", why)
     }
 }
 
 #[command]
-async fn valheim_status(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.channel_id.say(&ctx.http, ":construction:").await?;
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+    Ok(())
+}
+
+#[command]
+async fn status(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "command under :construction:").await?;
     Ok(())
 }
